790f11377a5eea993939d7d476bbffefbefb232d
Takaaki Saito <takaaki.skater@gmail.com>
2024-03-11 17:48:46 +0900

    initial commit

---
+++ Source/Component/HorizontalMeter.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <JuceHeader.h>
+
+namespace Gui {
+class HorizontalMeter : public juce::Component
+{
+public:
+    void paint (juce::Graphics& g) override
+    {
+        auto bounds = getLocalBounds().toFloat();
+        
+        g.setColour(juce::Colours::white.withBrightness(0.4f));
+        g.fillRoundedRectangle(bounds, 5.f);
+        
+        g.setColour(juce::Colours::white);
+//        map level from -60.f / +6.f to 0 / width
+        const auto scaledX = juce::jmap(level, -60.f, +6.f, 0.f, static_cast<float>(getWidth()));
+        g.fillRoundedRectangle(bounds.removeFromLeft(scaledX), 5.f);
+    }
+    void setLevel(const float val) { level = val; }
+    
+private:
+    float level = -60.0f;
+};
+}
---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+LevelMeterAudioProcessorEditor::LevelMeterAudioProcessorEditor (LevelMeterAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    
+    setSize (400, 300);
+}
+
+LevelMeterAudioProcessorEditor::~LevelMeterAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void LevelMeterAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+}
+
+void LevelMeterAudioProcessorEditor::resized()
+{
+
+}
